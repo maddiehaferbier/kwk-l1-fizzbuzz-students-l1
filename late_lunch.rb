efc5b2ae0d99529864515df20675be4988701b24
maddiehaferbier <madisonhaferbier@gmail.com>
2018-06-05 22:02:21 +0000

    automatically backed up by learn

--- late_lunch.rb
+++ late_lunch.rb
@@ -1,14 +1,14 @@
 def fizzbuzz(i)
-  if i % 3 == 0
-    puts "fitz"
-    elsif i % 5 == 0
-    puts "buzz"
     elsif i % 5 == 0 && i % 3 == 0
     puts "fizzbuzz"
+  if i % 3 == 0
+    puts "fizz"
+    elsif i % 5 == 0
+    puts "buzz"
 else 
   puts nil
   
 end
 end
 
-fizzbuzz(4)
+fizzbuzz(15)
